47e8898e80fe798166be1be409d0d15497135231
AcV134 <anirudh.c.vinayan@gmail.com>
2024-07-17 23:15:11 +0530

    Complete footer

--- public/src/footer.js
+++ public/src/footer.js
@@ -1,8 +1,19 @@
-console.log("footer is active");
-
-$("document").ready(()=>{$(".footer-link").hover((e)=>{
-    console.log(e.target);
+$("document").ready(()=>{
+    $(".footer-link").hover((e)=>{
     $(e.target).closest('a').addClass('footer-hover');
 },(e)=>{
     $(e.target).closest('a').removeClass('footer-hover');
-})});
+});
+});
+
+
+
+$(".icon-link").hover((e) => {
+    console.log(e.target);
+    $(e.target).closest("a").addClass('hop');
+    $(e.target).closest("a").find('.fg-primary').css('color', '#F92C85');
+    setTimeout(() => {
+        $(e.target).closest("a").removeClass('hop');
+        $(e.target).closest("a").find('.fg-primary').css('color', '#5EBEC4');
+    }, 600);
+});
