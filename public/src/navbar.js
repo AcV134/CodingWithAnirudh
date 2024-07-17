074735800e8d4a7ca3acf6afce829a8636b22eb8
AcV134 <anirudh.c.vinayan@gmail.com>
2024-07-17 14:01:15 +0530

    Finish navbar

---
+++ public/src/navbar.js
@@ -0,0 +1,24 @@
+// $(document).ready(function() {
+//     $('.navitem').hover(
+//       function() {
+//         // On mouse enter
+//         $(this).addClass('item-hover');
+//         $(this).find('.navlink').addClass('link-hover');
+//       }, function() {
+//         // On mouse leave
+//         $(this).removeClass('item-hover');
+//         $(this).find('.navlink').removeClass('link-hover');
+//       }
+//     );
+//   });
+
+console.log("reasder");
+
+$("document").ready(()=>{$(".nav-item").hover((e)=>{
+    console.log(e.target);
+    $(e.target).closest('li').addClass('item-hover');
+    $(e.target).closest('li').find('.nav-link').addClass('link-hover');
+},(e)=>{
+    $(e.target).closest('li').removeClass('item-hover');
+    $(e.target).closest('li').find('.nav-link').removeClass('link-hover');
+})});
