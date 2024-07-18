f92f9907a47bbe567a54a90b9dcf1ca69e618209
AcV134 <anirudh.c.vinayan@gmail.com>
2024-07-18 11:31:13 +0530

    Finalize framework

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ app.get("/",(req,res) =>{
     res.render("index.ejs");
 })
 
+app.get("/about",(req,res) =>{
+    res.render("about.ejs");
+})
+
+app.get("/blog",(req,res) =>{
+    res.render("blog.ejs");
+})
+
+app.get("/projects",(req,res) =>{
+    res.render("projects.ejs");
+})
+
 app.listen(port,() =>{
     console.log(`Server is running on port ${port}`);
 });
