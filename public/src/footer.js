4a13ff69275c647b022c975324b5d783afc846a0
AcV134 <anirudh.c.vinayan@gmail.com>
2024-07-18 11:21:28 +0530

    Add border to svg

--- public/src/footer.js
+++ public/src/footer.js
@@ -11,9 +11,11 @@ $("document").ready(()=>{
 $(".icon-link").hover((e) => {
     console.log(e.target);
     $(e.target).closest("a").addClass('hop');
-    $(e.target).closest("a").find('.fg-primary').css('color', '#F92C85');
+    $(e.target).closest("a").find('.fg-primary').css('fill', '#F92C85');
+    // $(e.target).closest("a").find('.fg-primary').css('box-shadow', '0 10px 10px rgba(0, 0, 0, 0.2)');
     setTimeout(() => {
         $(e.target).closest("a").removeClass('hop');
-        $(e.target).closest("a").find('.fg-primary').css('color', '#5EBEC4');
+        $(e.target).closest("a").find('.fg-primary').css('fill', '#5EBEC4');
+        // $(e.target).closest("a").find('.fg-primary').css('box-shadow', '0');   
     }, 600);
 });
