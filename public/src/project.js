3c0cb22f3a233b7bfe1ad0e630dbc752719ddec7
AcV134 <anirudh.c.vinayan@gmail.com>
2024-07-23 11:20:24 +0530

    Style project

---
+++ public/src/project.js
@@ -0,0 +1,11 @@
+$(document).ready(()=>{
+    $('.project').click((e)=>{
+        $(e.target).siblings().removeClass('project-click');
+        $(e.target).closest('.project').toggleClass('project-click');
+        $(e.target).closest('.project-container').addClass('project-container-click');
+        if($(e.target).hasClass('project-click')===false){
+            $(e.target).closest('.project-container').removeClass('project-container-click');
+        }
+    }
+)
+})
